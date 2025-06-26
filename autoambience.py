406e35b11c54cda576c969e49dd4d3405dc7e813
dora-explora <sarrafdara@gmail.com>
2025-06-26 13:17:23 -0700

    edited synth and voicings based off Somebody Real thank you Evan Alderete :3

--- autoambience.py
+++ autoambience.py
@@ -3,24 +3,44 @@ from time import sleep
 
 port = mido.open_output()
 
-C = 36
-E = 40
+C = 24
+Db = 25
+D = 26
+Eb = 27
+E = 28
+F = 29
+Gb = 30
+G = 31
+Ab = 32
+A = 33
+Bb = 34
+B = 35
 
-Maj7 = [0, 7, 16, 23, 28, 31, 35]
-Maj9 = [0, 7, 16, 23, 28, 31, 35, 38]
-pressures = [0, 4, 8, 13, 17, 22, 26, 31, 35, 39, 43, 47, 50, 54, 57, 61, 64, 67, 70, 73, 76, 79, 82, 85, 87, 90, 92, 94, 97, 99, 101, 103, 104, 106, 108, 109, 111, 112, 113, 114, 115, 116, 117, 118, 118, 119, 119, 119, 120, 120, 120, 120, 120, 119, 119, 119, 118, 118, 117, 116, 115, 114, 113, 112, 111, 109, 108, 106, 104, 103, 101, 99, 97, 94, 92, 90, 87, 85, 82, 79, 76, 73, 70, 67, 64, 61, 57, 54, 50, 47, 43, 39, 35, 31, 26, 22, 17, 13, 8, 4, 0]
+maj7 = [0, 7, 16, 23, 28, 31, 35]
+maj9 = [0, 7, 16, 23, 28, 31, 35, 38]
+# maj9 = [0, 12, 16, 19, 23, 24, 26, 28, 31, 35, 36, 38] alternate voicing, also yoinked from Somebody Real
+min7 = [0, 7, 15, 22, 27, 31, 34]
+min9 = [0, 7, 15, 22, 27, 31, 34, 38]
+min11 = [0, 7, 15, 22, 27, 31, 34, 38, 41]
 
 def play_chord(root: int, notes: list[int], delay: float):
     for note in notes:
         port.send(mido.Message('note_on', note=(note + root)))
 
-    for pressure in pressures:
+    for pressure in range(128):
         port.send(mido.Message('aftertouch', value=pressure))
         sleep(delay)
 
     for note in notes:
         port.send(mido.Message('note_off', note=(note + root)))
-    sleep(delay)
+    sleep(delay * 3)
 
-play_chord(C, Maj7, 0.1)
-play_chord(E, Maj9, 0.1)
+# reference: Somebody Real by Evan Alderete
+play_chord(Ab, maj9, 0.1)
+play_chord(G, min9, 0.1)
+
+# play_chord(F, maj7, 0.1)
+# play_chord(B, min11, 0.1)
+# play_chord(D, maj7, 0.11)
+# play_chord(Db, min7, 0.09)
+# play_chord(A, maj9, 0.1)
